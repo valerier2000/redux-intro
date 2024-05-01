4033feccb9959d9496ea5fbbefcc14c6a99ca1d6
Valeriia Rohatynska <valeriiarohatynska@gmail.com>
2024-05-01 15:46:43 +0200

    Add action creators

--- src/store.js
+++ src/store.js
@@ -35,13 +35,22 @@ function reducer(state = initialState, action) {
 
 const store = createStore(reducer);
 
-store.dispatch({ type: "account/deposit", payload: 500 });
-store.dispatch({ type: "account/withdraw", payload: 400 });
+function deposit(amount) {
+  return { type: "account/deposit", payload: amount };
+}
+function withdraw(amount) {
+  return { type: "account/withdraw", payload: amount };
+}
+function requestLoan(amount, purpose) {
+  return {
+    type: "account/requestLoan",
+    payload: { amount, purpose },
+  };
+}
+function payLoan() {
+  return { type: "account/payLoan" };
+}
 
-store.dispatch({
-  type: "account/requestLoan",
-  payload: { amount: 1000, purpose: "Buy a car" },
-});
-console.log(store.getState());
-store.dispatch({ type: "account/payLoan" });
+store.dispatch(deposit(500));
+store.dispatch(requestLoan(1000, "Buy cheap car"));
 console.log(store.getState());
