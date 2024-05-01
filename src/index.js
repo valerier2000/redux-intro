b3509f3e65413a42e96b65ff4e0494892bb5437f
Valeriia Rohatynska <valeriiarohatynska@gmail.com>
2024-05-01 16:45:49 +0200

    Connect Redux with App

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { Provider } from "react-redux";
 import "./index.css";
 import App from "./App";
 
@@ -8,6 +9,8 @@ import store from "./store";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>
 );
